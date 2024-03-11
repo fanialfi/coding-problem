a8660c8fccbca38e4cdadfe70895c9de86fe6306
fanialfi <fanialfirdaus@gmail.com>
2024-03-11 07:42:26 +0700

    tantangan coding 1/366

---
+++ 1_return.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55a5befdf16499bffb00007b/train/javascript
+
+const add = (a, b) => a + b;
+const divide = (a, b) => a / b || b / a;
+const multiply = (a, b) => a * b
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+const subt = (a, b) => a - b;
