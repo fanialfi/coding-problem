6c5f4d8235382de49858f372e5a62e5d3e81a375
fanialfi <fanialfirdaus@gmail.com>
2024-03-13 11:41:24 +0700

    tangangan coding 3/366

---
+++ js/3_hello_world.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+
+// function helloWorld() {
+//   const str = "Hello World!";
+//   console.log(str);
+// }
+
+const helloWorld = () => console.log("Hello World!");
