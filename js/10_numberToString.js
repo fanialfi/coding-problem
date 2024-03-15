be97189e9385df09384527e21d2fb0582b33359e
fanialfi <fanialfirdaus@gmail.com>
2024-03-15 16:28:22 +0700

    tantangan coding 10/366

---
+++ js/10_numberToString.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// function numberToString(num) {
+//   return num.toString()
+// }
+
+const numberToString = (num) => num.toString();
