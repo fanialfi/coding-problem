d3bc617be2614226970ce7d3bfc65df298528fd4
fanialfi <fanialfirdaus@gmail.com>
2024-03-21 23:42:34 +0700

    tantangan coding 26/366

--- js/25_greet.js
+++ js/25_greet.js
@@ -4,4 +4,8 @@
 //   return `Hello, ${name} how are you doing today?`
 // }
 
-const greet = (name) => `Hello, ${name} how are you doing today?`
+let greet = (name) => `Hello, ${name} how are you doing today?`
+
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+greet = () => "hello world!"
