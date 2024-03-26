4f03a700346101620193b433205410f4c2b19287
fanialfi <fanialfirdaus@gmail.com>
2024-03-26 07:57:40 +0700

    tantangan coding 31/366

---
+++ js/31_booleanToString.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/
+
+// function booleanToString(b){
+//   if(b){
+//     return "true"
+//   }else{
+//     return "false"
+//   }
+// }
+
+// function booleanToString(b){
+//   return b ? "true":"false"
+// }
+
+// const booleanToString = (b) => b ? "true" : "false"
+
+const booleanToString = (b) => b.toString();
