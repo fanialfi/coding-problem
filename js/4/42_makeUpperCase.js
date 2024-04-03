bac67040fa44b5d069a007cb9a07a1de172f8e7e
fanialfi <fanialfirdaus@gmail.com>
2024-04-03 07:54:18 +0700

    tantangan coding 42/366

---
+++ js/4/42_makeUpperCase.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/
+//
+// tantangan coding 42/366
+
+// function makeUpperCase(str) {
+//   return str.toUpperCase();
+// }
+
+const makeUpperCase = (str) => str.toUpperCase();
+
+console.log(makeUpperCase(""));
