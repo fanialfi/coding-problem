63d1e6cbab838dcbfc311dd19c6fb4286c566186
fanialfi <fanialfirdaus@gmail.com>
2024-04-03 08:24:07 +0700

    tantangan coding 44/366

---
+++ js/4/44_smash.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/
+//
+// tantangan coding 44/366
+
+// function smash(words) {
+//   return words.join(" ");
+// }
+
+const smash = (words) => words.join(" ");
+
+console.log(smash([]));
+console.log(smash(["hello", "world"]));
