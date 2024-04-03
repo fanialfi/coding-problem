9d7dfe93215f55379129605c1455964bbfed277b
fanialfi <fanialfirdaus@gmail.com>
2024-04-03 21:57:52 +0700

    tantangan coding 45/366

---
+++ js/4/45_hero.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/
+//
+// tantangan coding 45/366
+
+// function hero(bullets, dragons){
+//   if(bullets / 2 >= dragons){
+//     return true;
+//   }
+//   return false
+// }
+
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons ? true : false;
+// }
+
+const hero = (bullets, dragons) => (bullets / 2 >= dragons ? true : false);
