d69354ce7875f451aece8b82dcc35f9d4dd8cd40
fanialfi <fanialfirdaus@gmail.com>
2024-04-05 01:12:11 +0700

    tantangan coding 48/366

---
+++ js/4/48_doubleInteger.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/
+//
+// tantangan coding 48/366
+
+// function doubleInteger(i) {
+//   // i will be an integer. Double it and return it.
+//   return i * 2;
+// }
+
+const doubleInteger = (i) => i * 2;
