dd107e07d74d3e95131a350e8e60ab07ca72325c
fanialfi <fanialfirdaus@gmail.com>
2024-04-09 07:36:52 +0700

    tantangan coding 53/366

---
+++ js/4/53_findMaxMin.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989/
+//
+// tantangan coding 53/366
+
+// function min(list) {
+//   return Math.min(...list);
+// }
+
+// function max(list) {
+//   return Math.max(...list);
+// }
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566]));
