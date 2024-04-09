695bb78984bee499b77aeb4b94845f406c36732b
fanialfi <fanialfirdaus@gmail.com>
2024-04-09 07:17:32 +0700

    tantangan codoing 51/366

---
+++ js/4/51_greet.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/
+//
+// tantangan codoing 51/366
+
+// function greet(name) {
+//   return name == "Johnny" ? "Hello, my love!" : `Hello, ${name}!`;
+// }
+
+const greet = (name) =>
+  name == "Johnny" ? "Hello, my love!" : `Hello, ${name}!`;
