3a84c964818f32f79e6a74b8ef1d19dafbb28314
fanialfi <fanialfirdaus@gmail.com>
2024-04-19 06:59:31 +0700

    tantangan coding 62/366

---
+++ js/4/62_sumMix.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009/
+//
+// tantangan coding 62/366
+
+// function sumMix(x) {
+//   return x.reduce((acc, cur) => +acc + +cur, 0);
+// }
+
+const sumMix = (x) => x.reduce((acc, cur) => +acc + +cur, 0);
