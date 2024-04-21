66d38ed62dadc6c71ae00058689d388ee83e1b95
fanialfi <fanialfirdaus@gmail.com>
2024-04-21 10:51:56 +0700

    tantangan coding 63/366

---
+++ js/4/63_removeExclamationMarks.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/
+//
+// tantangan coding 63/366
+
+// function removeExclamationMarks(s) {
+//   return s.split("!").join("");
+// }
+
+const removeExclamationMarks = (s) => s.split("!").join("");
+
+console.log(removeExclamationMarks("Hello World!"));
