9a13d89ea0bb71165c811011dda4658d90f141cc
fanialfi <fanialfirdaus@gmail.com>
2024-05-01 07:14:42 +0700

    coding chalenge 74/366

---
+++ js/5/74_otherAngle.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5a023c426975981341000014/
+//
+// coding chalenge 74/366
+
+// function otherAngle(a, b) {
+//   return 180 - (a + b);
+// }
+
+const otherAngle = (a, b) => 180 - (a + b);
+
+console.log(otherAngle(30, 60));
