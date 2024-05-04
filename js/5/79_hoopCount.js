09028cc0c3f09d2e623356961538098aa9f3c33c
fanialfi <fanialfirdaus@gmail.com>
2024-05-05 00:30:16 +0700

    coding chalenge 79/366

---
+++ js/5/79_hoopCount.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/
+//
+// coding chalenge 79/366
+
+// function hoopCount(n) {
+//   if (n >= 10) {
+//     return "Great, now move on to tricks";
+//   } else {
+//     return "Keep at it until you get it";
+//   }
+// }
+
+const hoopCount = (n) => (n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it");
