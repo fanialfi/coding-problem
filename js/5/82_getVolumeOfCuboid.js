f8f3c7bd3e2678ec88daffd4c202db02a5961be1
fanialfi <fanialfirdaus@gmail.com>
2024-05-08 05:21:32 +0700

    coding chalenge 82/366

---
+++ js/5/82_getVolumeOfCuboid.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/
+//
+// coding chalenge 82/366
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+console.log(Kata.getVolumeOfCuboid(1, 2, 2)); // 4
