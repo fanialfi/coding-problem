f6ef7416246ada7e7bb948080f42f3565e638aa8
fanialfi <fanialfirdaus@gmail.com>
2024-05-30 07:02:55 +0700

    coding chalenge 105/366

---
+++ js/5/105_addBinary.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/551f37452ff852b7bd000139
+//
+// coding chalenge 105/366
+
+// function addBinary(a, b) {
+//   return (Number(a) + Number(b)).toString(2);
+// }
+
+const addBinary = (a, b) => (Number(a) + Number(b)).toString(2);
