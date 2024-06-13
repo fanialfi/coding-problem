e0e0cb974bf7df03e954a9cbe98e44ed38110e6e
fanialfi <fanialfirdaus@gmail.com>
2024-06-13 12:18:38 +0700

    coding chalenge 120/366

---
+++ js/6/120_breakChocolate.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/534ea96ebb17181947000ada
+//
+// coding chalenge 120/366
+
+// function breakChocolate(n, m) {
+//   if (n && m > 0) {
+//     return n * m - 1;
+//   } else {
+//     return 0;
+//   }
+// }
+
+const breakChocolate = (n, m) => (n && m > 0 ? n * m - 1 : 0);
