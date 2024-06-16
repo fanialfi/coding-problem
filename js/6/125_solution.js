9619605212bfed557080dc9252a22bdfc127cba6
fanialfi <fanialfirdaus@gmail.com>
2024-06-16 15:53:05 +0700

    coding chalenge 125/366

---
+++ js/6/125_solution.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
+//
+// coding chalenge 125/366
+
+// function solution(nums) {
+//   return nums == null ? [] : nums.sort((a, b) => a - b);
+// }
+
+const solution = (nums) => (nums == null ? [] : nums.sort((a, b) => a - b));
